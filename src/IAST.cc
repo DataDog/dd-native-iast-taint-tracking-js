c98694f29effc5736011342aee91c06cdc1cb7e2
Julio Gonzalez <julio.gonzalez@datadoghq.com>
2022-08-17 17:21:06 +0200

    Solve PR comments.

--- src/IAST.cc
+++ src/IAST.cc
@@ -5,14 +5,6 @@
 namespace IAST {
 
     void Init(v8::Local<v8::Object> exports) {
-      /* equals::Equals::Init(exports); */
-      /* tainted::StringMethods::Init(exports); */
-      /* operations::PlusOperations::Init(exports); */
-      /* operations::StringReplace::Init(exports); */
-      /* operations::StringSubstring::Init(exports); */
-      /* operations::StringSlice::Init(exports); */
-      /* operations::ArrayJoin::Init(exports); */
-      /* tainted::RangesManager::Init(exports); */
     }
 
     NODE_MODULE(NODE_GYP_MODULE_NAME, Init);

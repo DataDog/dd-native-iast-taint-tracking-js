e618b526de9608efe294739ccd07f7bcfebd5f82
Julio Gonzalez <julio.gonzalez@datadoghq.com>
2022-10-06 12:36:54 +0200

    Add interface in index.js.

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const os = require('os')
 const path = require('path')
-const getLibc = require('./scripts/libc')
+const getLibc = require(path.join(__dirname, 'scripts', 'libc'))
 const archFolder = `${os.platform()}-${os.arch()}-${getLibc()}` + '-' + process.versions.node.split('.')[0]
 const pathToAddon = path.join('dist', archFolder, 'iastnativemethods.node')
 
-module.exports = require(`./${pathToAddon}`)
+let addon = require(path.join(__dirname, pathToAddon))
+
+let iastNativeMethods = {
+    newTaintedString: addon.newTaintedString,
+    isTainted: addon.isTainted,
+    getRanges : addon.getRanges,
+    createTransaction : addon.createTransaction,
+    removeTransaction : addon.removeTransaction,
+    concat : addon.concat
+}
+
+module.exports = iastNativeMethods
--- scripts/pack.js
+++ scripts/pack.js
@@ -11,11 +11,3 @@ archFolder += '-' + process.versions.node.split('.')[0]
 
 fs.mkdirSync(path.join(__dirname, '..', 'dist', archFolder), { recursive: true })
 fs.renameSync(path.join(__dirname, '..', 'dist', 'iastnativemethods.node'), path.join(__dirname, '..', 'dist', archFolder, 'iastnativemethods.node'))
-
-// tar.c({
-//     file: path.join(__dirname, '..', `${os.platform()}-${os.arch()}-${getLibc()}.tgz`),
-//     gzip:true
-//     },
-//     [path.join(__dirname,  '..', 'dist')]
-// )
-

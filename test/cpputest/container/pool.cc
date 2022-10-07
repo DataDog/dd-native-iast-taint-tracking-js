acd137c8cf1b82c553e7f6c03040b19d1c56f576
Julio Gonzalez <julio.gonzalez@datadoghq.com>
2022-10-07 12:56:28 +0200

    Add license to source test files.

--- test/cpputest/container/pool.cc
+++ test/cpputest/container/pool.cc
@@ -1,3 +1,7 @@
+/**
+* Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+* This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2022 Datadog, Inc.
+**/
 #include "container/pool.h"
 
 #include <iostream>

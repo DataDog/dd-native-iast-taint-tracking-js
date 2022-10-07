da8bc3f37ed1ac62f247c9d39304f906aa15a6f6
Julio Gonzalez <julio.gonzalez@datadoghq.com>
2022-10-07 12:45:40 +0200

    Add license to source files.

--- src/transaction_manager.h
+++ src/transaction_manager.h
@@ -1,6 +1,9 @@
+/**
+* Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+* This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2022 Datadog, Inc.
+**/
 #ifndef SRC_TRANSACTION_MANAGER_H_
 #define SRC_TRANSACTION_MANAGER_H_
-// Copyright 2022 Datadog, Inc.
 #include <cstdint>
 #include <map>
 #include <iostream>

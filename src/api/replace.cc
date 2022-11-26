b915af5cb594846ff363006f0ae7d0806cf3eecc
Ugaitz Urien <ugaitz.urien@datadoghq.com>
2022-11-26 17:15:50 +0100

    Refactor: Trying to reduce long methods

--- src/api/replace.cc
+++ src/api/replace.cc
@@ -28,113 +28,242 @@ struct JsReplacementInfo {
     int64_t matchLength;
     int64_t offset;
 };
+
+struct StringMatcherArguments {
+    Transaction* transaction;
+    v8::Local<Value> result;
+    v8::Local<Value> self;
+    v8::Local<Value> matcher;
+    v8::Local<Value> replacer;
+    v8::Local<Value> replacement;
+};
+
+struct RegexMatcherArguments {
+    Transaction* transaction;
+    v8::Local<Value> result;
+    v8::Local<Value> self;
+    v8::Local<Value> matcher;
+    v8::Local<Value> replacer;
+    v8::Local<Value> replacements;
+};
+
+inline StringMatcherArguments ReadStringMatcherMethodArguments(const FunctionCallbackInfo<Value>& args) {
+    auto replaceResult = args[1];
+    if (!replaceResult->IsString()) {
+        return StringMatcherArguments {
+            nullptr, replaceResult, args[2], args[3], args[4], args[5]
+        };
+    } else {
+        auto transaction = GetTransaction(utils::GetLocalStringPointer(args[0]));
+        return StringMatcherArguments {
+                transaction, replaceResult, args[2], args[3], args[4], args[5]
+        };
+    }
+}
+
+inline RegexMatcherArguments ReadRegexMatcherMethodArguments(const FunctionCallbackInfo<Value>& args) {
+    auto replaceResult = args[1];
+    if (!replaceResult->IsString()) {
+        return RegexMatcherArguments {
+            nullptr, replaceResult, args[2], args[3], args[4], args[5]
+        };
+    } else {
+        auto transaction = GetTransaction(utils::GetLocalStringPointer(args[0]));
+        return RegexMatcherArguments {
+                transaction, replaceResult, args[2], args[3], args[4], args[5]
+        };
+    }
+}
+
+inline SharedRanges* GetTaintedRangesFromLocal(Transaction* transaction, v8::Local<Value> value) {
+    auto pointer = utils::GetLocalStringPointer(value);
+    auto taintedObject = transaction->FindTaintedObject(pointer);
+    if (taintedObject) {
+        return taintedObject->getRanges();
+    }
+    return nullptr;
+}
+
+#define INIT_STRING_MATCHER_PROPERTIES \
+auto matcherLength = v8::String::Cast(*(methodArguments.matcher))->Length(); \
+auto replacementLength = v8::String::Cast(*(methodArguments.replacer))->Length(); \
+auto toReplaceStart = v8::Integer::Cast(*(methodArguments.replacement))->Value(); \
+int toReplaceEnd = toReplaceStart + matcherLength; \
+int offset = replacementLength - matcherLength; \
+auto newRanges = methodArguments.transaction->GetSharedVectorRange();
+
+#define ADD_BEFORE_REPLACEMENT_RANGES \
+while (subjectIt != subjectItEnd && (*subjectIt)->start < toReplaceStart) { \
+    auto range = (*subjectIt); \
+    if (range->end <= toReplaceStart) { \
+        newRanges->PushBack(range); \
+    } else if (range->end > toReplaceStart) { \
+        newRanges->PushBack(methodArguments.transaction->GetRange(range->start, toReplaceStart, range->inputInfo)); \
+        break; \
+    } \
+    ++subjectIt; \
+}
+
+#define ADD_REPLACEMENT_RANGES \
+if (replacerRanges) { \
+    if (toReplaceStart == 0) { \
+        newRanges->Add(replacerRanges); \
+    } else { \
+        auto replacerItEnd = replacerRanges->end(); \
+        for (auto replacerIt = replacerRanges->begin(); replacerItEnd != replacerIt; replacerIt++) { \
+            auto range = (*replacerIt); \
+            newRanges->PushBack(methodArguments.transaction->GetRange(range->start + toReplaceStart, \
+            range->end + toReplaceStart, range->inputInfo)); \
+        } \
+    } \
+}
+
+#define ADD_AFTER_REPLACEMENT_RANGES \
+while (subjectIt != subjectItEnd) { \
+    auto range = (*subjectIt); \
+    if (range->end > toReplaceEnd) { \
+        if (range->start <= toReplaceEnd) { \
+            newRanges->PushBack(methodArguments.transaction->GetRange(toReplaceEnd + offset, \
+                range->end + offset, \
+                range->inputInfo)); \
+        } else if (offset == 0) { \
+            newRanges->PushBack(range); \
+        } else { \
+            newRanges->PushBack(methodArguments.transaction->GetRange(range->start + offset, \
+                range->end + offset, \
+                range->inputInfo)); \
+        } \
+    } \
+    subjectIt++; \
+}
+
 void TaintReplaceStringByStringMethod(const FunctionCallbackInfo<Value>& args) {
-    // transactionId, result, subject, matcher, replacer, index
     if (!utils::ValidateMethodArguments(args, 6, "Wrong number of arguments")) {
         return;
     }
-    auto isolate = args.GetIsolate();
     auto replaceResult = args[1];
     if (!replaceResult->IsString()) {
         args.GetReturnValue().Set(replaceResult);
         return;
     }
-
-    auto transactionId = args[0];
-    auto self = args[2];
-    auto matcher = args[3];
-    auto replacer = args[4];
-    auto replacement = args[5];
-
     try {
-        auto transaction = GetTransaction(utils::GetLocalStringPointer(transactionId));
-        if (transaction == nullptr) {
-            args.GetReturnValue().Set(args[1]);
+        auto methodArguments = ReadStringMatcherMethodArguments(args);
+        if (methodArguments.transaction == nullptr) {
+            args.GetReturnValue().Set(methodArguments.result);
             return;
         }
-        auto subjectPointer = utils::GetLocalStringPointer(self);
-        auto replacerPointer = utils::GetLocalStringPointer(replacer);
 
-        auto subjectTaintedObject = transaction->FindTaintedObject(subjectPointer);
-        auto replacerTaintedObject = transaction->FindTaintedObject(replacerPointer);
-        auto subjectRanges = subjectTaintedObject ? subjectTaintedObject->getRanges() : nullptr;
-        auto replacerRanges = replacerTaintedObject ? replacerTaintedObject->getRanges() : nullptr;
-
+        auto subjectRanges = GetTaintedRangesFromLocal(methodArguments.transaction, methodArguments.self);
+        auto replacerRanges = GetTaintedRangesFromLocal(methodArguments.transaction, methodArguments.replacer);
         if (subjectRanges == nullptr && replacerRanges == nullptr) {
             args.GetReturnValue().Set(replaceResult);
             return;
         }
 
-        auto matcherLength = v8::String::Cast(*matcher)->Length();
-        auto replacementLength = v8::String::Cast(*replacer)->Length();
-        auto toReplaceStart = v8::Integer::Cast(*replacement)->Value();
-        int toReplaceEnd = toReplaceStart + matcherLength;
-        int offset = replacementLength - matcherLength;
-        auto newRanges = transaction->GetSharedVectorRange();
+        INIT_STRING_MATCHER_PROPERTIES
+
         std::vector<Range*>::iterator subjectIt;
         std::vector<Range*>::iterator subjectItEnd;
         if (subjectRanges) {
             subjectIt = subjectRanges->begin();
             subjectItEnd = subjectRanges->end();
-            while (subjectIt != subjectItEnd && (*subjectIt)->start < toReplaceStart) {
-                auto range = (*subjectIt);
-                if (range->end <= toReplaceStart) {
-                    newRanges->PushBack(range);
-                } else if (range->end > toReplaceStart) {
-                    newRanges->PushBack(transaction->GetRange(range->start, toReplaceStart, range->inputInfo));
-                    break;
-                }
-                ++subjectIt;
-            }
+            ADD_BEFORE_REPLACEMENT_RANGES
         }
 
-        if (replacerRanges) {
-            if (toReplaceStart == 0) {
-                newRanges->Add(replacerRanges);
-            } else {
-                auto replacerItEnd = replacerRanges->end();
-                for (auto replacerIt = replacerRanges->begin(); replacerItEnd != replacerIt; replacerIt++) {
-                    auto range = (*replacerIt);
-                    newRanges->PushBack(transaction->GetRange(range->start + toReplaceStart,
-                                                              range->end + toReplaceStart, range->inputInfo));
-                }
-            }
-        }
+        ADD_REPLACEMENT_RANGES
 
         if (subjectRanges) {
-            while (subjectIt != subjectItEnd) {
-                auto range = (*subjectIt);
-                if (range->end > toReplaceEnd) {
-                    if (range->start <= toReplaceEnd) {
-                        newRanges->PushBack(transaction->GetRange(toReplaceEnd + offset,
-                                                                  range->end + offset,
-                                                                  range->inputInfo));
-                    } else if (offset == 0) {
-                        newRanges->PushBack(range);
-                    } else {
-                        newRanges->PushBack(transaction->GetRange(range->start + offset,
-                                                                  range->end + offset,
-                                                                  range->inputInfo));
-                    }
-                }
-                subjectIt++;
-            }
+            ADD_AFTER_REPLACEMENT_RANGES
         }
 
         if (newRanges->Size() > 0) {
+            auto isolate = args.GetIsolate();
             auto resultString = replaceResult->ToString(isolate->GetCurrentContext()).ToLocalChecked();
             auto resultLength = resultString->Length();
             if (resultLength == 1) {
                 replaceResult = tainted::NewExternalString(isolate, replaceResult);
             }
             auto key = utils::GetLocalStringPointer(replaceResult);
-            transaction->AddTainted(key, newRanges, replaceResult);
+            methodArguments.transaction->AddTainted(key, newRanges, replaceResult);
         }
     } catch (const std::bad_alloc& err) {
     } catch (const container::QueuedPoolBadAlloc& err) {
     } catch (const container::PoolBadAlloc& err) {
     }
     args.GetReturnValue().Set(replaceResult);
+}
+#undef INIT_STRING_MATCHER_PROPERTIES
+#undef ADD_BEFORE_REPLACEMENT_RANGES
+#undef ADD_REPLACEMENT_RANGES
+#undef ADD_AFTER_REPLACEMENT_RANGES
+
+#define INIT_REGEX_REPLACE_PROPERTIES \
+auto isolate = args.GetIsolate(); \
+auto context = isolate->GetCurrentContext(); \
+auto jsReplacements = v8::Array::Cast(*replacements); \
+auto jsReplacementsLength = jsReplacements->Length(); \
+auto replacerLength = v8::String::Cast(*(methodArguments.replacer))->Length(); \
+std::vector<Range*>::iterator subjectIt; \
+std::vector<Range*>::iterator subjectItEnd; \
+
+#define ADD_RANGES_BEFORE_REPLACEMENT \
+while (subjectIt != subjectItEnd && (*subjectIt)->start < index) { \
+    auto breakLoop = false; \
+    auto range = *subjectIt; \
+    if (lastEnd < range->end) { \
+        auto start = range->start; \
+        auto end = range->end; \
+        if (lastEnd > range->start) { \
+            start = lastEnd; \
+        } \
+        start += offset; \
+        if (range->end > index) { \
+            breakLoop = true; \
+            end = index; \
+        } \
+        end += offset; \
+        if (start == range->start && end == range->end) { \
+            newRanges->PushBack(range); \
+        } else { \
+            newRanges->PushBack(methodArguments.transaction->GetRange(start, end, range->inputInfo)); \
+        } \
+    } \
+    if (breakLoop) { \
+        break; \
+    } \
+    ++subjectIt; \
+}
+#define ADD_REPLACEMENT_RANGES \
+if (replacerRanges != nullptr) { \
+    auto toReplaceStart =  index + offset; \
+    if (toReplaceStart == 0) { \
+        newRanges->Add(replacerRanges); \
+    } else { \
+        auto replacerItEnd = replacerRanges->end(); \
+        for (auto replacerIt = replacerRanges->begin(); replacerItEnd != replacerIt; replacerIt++) { \
+            auto range = (*replacerIt); \
+            newRanges->PushBack(methodArguments.transaction->GetRange(range->start + toReplaceStart, \
+                range->end + toReplaceStart, range->inputInfo)); \
+        } \
+    } \
+}
+
+#define ADD_RANGES_AFTER_ALL_REPLACEMENTS \
+while (subjectIt != subjectItEnd) { \
+    auto range = *subjectIt; \
+    if (lastEnd < range->end) { \
+        if (lastEnd > range->start) { \
+            newRanges->PushBack(methodArguments.transaction->GetRange(lastEnd + offset, range->end + offset, \
+                range->inputInfo)); \
+        } else if (offset == 0) { \
+            newRanges->PushBack(range); \
+        } else { \
+            newRanges->PushBack( \
+                methodArguments.transaction->GetRange(range->start + offset, range->end + offset, \
+                range->inputInfo)); \
+        } \
+    } \
+    ++subjectIt; \
 }
 
 void TaintReplaceStringByStringUsingRegexMethod(const FunctionCallbackInfo<Value>& args) {
@@ -147,43 +276,30 @@ void TaintReplaceStringByStringUsingRegexMethod(const FunctionCallbackInfo<Value
         args.GetReturnValue().Set(replaceResult);
         return;
     }
-    auto transactionId = args[0];
-    auto self = args[2];
-    auto matcher = args[3];
-    auto replacer = args[4];
     auto replacements = args[5];
 
     try {
-        auto transaction = GetTransaction(utils::GetLocalStringPointer(transactionId));
-        if (transaction == nullptr) {
+        auto methodArguments = ReadRegexMatcherMethodArguments(args);
+        if (methodArguments.transaction == nullptr) {
             args.GetReturnValue().Set(replaceResult);
             return;
         }
-        auto subjectPointer = utils::GetLocalStringPointer(self);
-        auto replacerPointer = utils::GetLocalStringPointer(replacer);
+        auto subjectRanges = GetTaintedRangesFromLocal(methodArguments.transaction, methodArguments.self);
+        auto replacerRanges = GetTaintedRangesFromLocal(methodArguments.transaction, methodArguments.replacer);
 
-        auto subjectTaintedObject = transaction->FindTaintedObject(subjectPointer);
-        auto replacerTaintedObject = transaction->FindTaintedObject(replacerPointer);
-        auto subjectRanges = subjectTaintedObject ? subjectTaintedObject->getRanges() : nullptr;
-        auto replacerRanges = replacerTaintedObject ? replacerTaintedObject->getRanges() : nullptr;
         if (subjectRanges == nullptr && replacerRanges == nullptr) {
             args.GetReturnValue().Set(replaceResult);
             return;
         }
-        auto isolate = args.GetIsolate();
-        auto context = isolate->GetCurrentContext();
-        auto jsReplacements = v8::Array::Cast(*replacements);
-        auto jsReplacementsLength = jsReplacements->Length();
-        auto replacerLength = v8::String::Cast(*replacer)->Length();
-        std::vector<Range*>::iterator subjectIt;
-        std::vector<Range*>::iterator subjectItEnd;
+
+        INIT_REGEX_REPLACE_PROPERTIES
 
         if (subjectRanges != nullptr) {
             subjectIt = subjectRanges->begin();
             subjectItEnd = subjectRanges->end();
         }
 
-        auto newRanges = transaction->GetSharedVectorRange();
+        auto newRanges = methodArguments.transaction->GetSharedVectorRange();
         int offset = 0;
         int lastEnd = 0;
         for (int i = 0; i < jsReplacementsLength; i++) {
@@ -191,66 +307,18 @@ void TaintReplaceStringByStringUsingRegexMethod(const FunctionCallbackInfo<Value
             auto index = v8::Int32::Cast(*(jsReplacement->Get(context, 0).ToLocalChecked()))->Value();
             auto matcherLength = v8::String::Cast(*(jsReplacement->Get(context, 1).ToLocalChecked()))->Length();
             struct JsReplacementInfo currentReplacement = {
-                    index, matcherLength, replacerLength - matcherLength
+                index, matcherLength, replacerLength - matcherLength
             };
             if (subjectRanges != nullptr) {
-                while (subjectIt != subjectItEnd && (*subjectIt)->start < index) {
-                    auto breakLoop = false;
-                    auto range = *subjectIt;
-                    if (lastEnd < range->end) {
-                        auto start = range->start;
-                        auto end = range->end;
-                        if (lastEnd > range->start) {
-                            start = lastEnd;
-                        }
-                        start += offset;
-                        if (range->end > index) {
-                            breakLoop = true;
-                            end = index;
-                        }
-                        end += offset;
-                        if (start == range->start && end == range->end) {
-                            newRanges->PushBack(range);
-                        } else {
-                            newRanges->PushBack(transaction->GetRange(start, end, range->inputInfo));
-                        }
-                    }
-                    if (breakLoop) {
-                        break;
-                    }
-                    ++subjectIt;
-                }
+                ADD_RANGES_BEFORE_REPLACEMENT
             }
-            if (replacerRanges != nullptr) {
-                auto toReplaceStart =  index + offset;
-                if (toReplaceStart == 0) {
-                    newRanges->Add(replacerRanges);
-                } else {
-                    auto replacerItEnd = replacerRanges->end();
-                    for (auto replacerIt = replacerRanges->begin(); replacerItEnd != replacerIt; replacerIt++) {
-                        auto range = (*replacerIt);
-                        newRanges->PushBack(transaction->GetRange(range->start + toReplaceStart,
-                                                                  range->end + toReplaceStart, range->inputInfo));
-                    }
-                }
-            }
+            ADD_REPLACEMENT_RANGES
+
             lastEnd = currentReplacement.index + currentReplacement.matchLength;
             offset = offset + currentReplacement.offset;
         }
-        while (subjectIt != subjectItEnd) {
-            auto range = *subjectIt;
-            if (lastEnd < range->end) {
-                if (lastEnd > range->start) {
-                    newRanges->PushBack(transaction->GetRange(lastEnd + offset, range->end + offset,
-                                                              range->inputInfo));
-                } else if (offset == 0) {
-                    newRanges->PushBack(range);
-                } else {
-                    newRanges->PushBack(transaction->GetRange(range->start + offset, range->end + offset,
-                                                              range->inputInfo));
-                }
-            }
-            ++subjectIt;
+        if (subjectRanges != nullptr) {
+            ADD_RANGES_AFTER_ALL_REPLACEMENTS
         }
 
         if (newRanges->Size() > 0) {
@@ -260,7 +328,7 @@ void TaintReplaceStringByStringUsingRegexMethod(const FunctionCallbackInfo<Value
                 replaceResult = tainted::NewExternalString(isolate, replaceResult);
             }
             auto key = utils::GetLocalStringPointer(replaceResult);
-            transaction->AddTainted(key, newRanges, replaceResult);
+            methodArguments.transaction->AddTainted(key, newRanges, replaceResult);
         }
     } catch (const std::bad_alloc& err) {
     } catch (const container::QueuedPoolBadAlloc& err) {

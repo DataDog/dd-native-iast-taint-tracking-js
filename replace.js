0f6386f6145060eff544920c93e771d624278bab
Ugaitz Urien <ugaitz.urien@datadoghq.com>
2022-12-12 12:15:53 +0100

    Comment in PR

--- replace.js
+++ replace.js
@@ -1,38 +1,39 @@
 const isSpecialRegex = /(\$\$)|(\$&)|(\$`)|(\$')|(\$\d)/
+
 function getReplace (addon) {
+  function isSpecialReplacement (replacer) {
+    return replacer.indexOf('$') > -1 && !!replacer.match(isSpecialRegex)
+  }
+  function shouldBePropagated (transactionId, thisArg, replacer) {
+    return addon.isTainted(transactionId, thisArg, replacer) && !isSpecialReplacement(replacer)
+  }
   if (addon.replace) {
     return addon.replace
   }
   return function replace (transactionId, result, thisArg, matcher, replacer) {
     if (transactionId && typeof thisArg === 'string' && typeof replacer === 'string') {
       if (typeof matcher === 'string') {
-        if (addon.isTainted(transactionId, thisArg, replacer)) {
-          const isSpecialReplacement = replacer.indexOf('$') > -1 && !!replacer.match(isSpecialRegex)
-          if (!isSpecialReplacement) {
-            const index = thisArg.indexOf(matcher)
-            if (index > -1) {
-              return addon.replaceStringByString(transactionId, result, thisArg, matcher, replacer, index)
-            }
+        if (shouldBePropagated(transactionId, thisArg, replacer)) {
+          const index = thisArg.indexOf(matcher)
+          if (index > -1) {
+            return addon.replaceStringByString(transactionId, result, thisArg, matcher, replacer, index)
           }
         }
       } else if (matcher instanceof RegExp) {
-        if (addon.isTainted(transactionId, thisArg, replacer)) {
-          const isSpecialReplacement = replacer.indexOf('$') > -1 && !!replacer.match(isSpecialRegex)
-          if (!isSpecialReplacement) {
-            const replacements = []
-            let lastIndex = -1
-            for (let match = matcher.exec(thisArg), i = 0; match != null; match = matcher.exec(thisArg), i++) {
-              const index = match.index
-              if (index !== lastIndex) {
-                replacements.push([index, match[0]])
-                lastIndex = index
-              } else {
-                break
-              }
+        if (shouldBePropagated(transactionId, thisArg, replacer)) {
+          const replacements = []
+          let lastIndex = -1
+          for (let match = matcher.exec(thisArg), i = 0; match != null; match = matcher.exec(thisArg), i++) {
+            const index = match.index
+            if (index !== lastIndex) {
+              replacements.push([index, match[0]])
+              lastIndex = index
+            } else {
+              break
             }
-            return addon.replaceStringByStringUsingRegex(transactionId, result, thisArg, matcher,
-              replacer, replacements)
           }
+          return addon.replaceStringByStringUsingRegex(transactionId, result, thisArg, matcher,
+            replacer, replacements)
         }
       }
     }

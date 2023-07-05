3db075d25235aa237b591fe08d57f82163e341c9
Ugaitz Urien <ugaitz.urien@datadoghq.com>
2023-07-05 11:40:15 +0200

    Change int to secure_marks_t

--- src/tainted/transaction.h
+++ src/tainted/transaction.h
@@ -39,7 +39,7 @@ class Transaction {
             v8::Local<v8::Value> parameterValue,
             v8::Local<v8::Value> type);
 
-    Range* GetRange(int start, int end, InputInfo *inputInfo, int secureMarks) {
+    Range* GetRange(int start, int end, InputInfo *inputInfo, secure_marks_t secureMarks) {
         return _rangesPool.Pop(start, end, inputInfo, secureMarks);
     }
 
